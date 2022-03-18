afc0d6dde992ec5e0f8e61f8c3b2ed0a6f3880e6
Jang-DongHo <donghoo18@naver.com>
2022-03-18 21:12:21 +0900

    return

--- src/javascript/loadExcel.js
+++ src/javascript/loadExcel.js
@@ -1,10 +1,10 @@
-import { readFile, utils } from "xlsx";
+const xlsx = require("xlsx");
 
-const excelFile = readFile("bus.xlsx");
+const excelFile = xlsx.readFile("bus.xlsx");
 
 const sheetName = excelFile.SheetNames[0];
 const firstSheet = excelFile.Sheets[sheetName];
 
 const jsonData = xlsx.utils.sheet_to_json(firstSheet, { defval: "" });
 
-console.log(jsonData[0].도착);
+console.log(jsonData);

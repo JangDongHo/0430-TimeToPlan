cdcb803abe6d05d7295f766b8b144c268c554b93
Jang-DongHo <donghoo18@naver.com>
2022-03-18 20:50:55 +0900

    updateExcel

--- src/javascript/loadExcel.js
+++ src/javascript/loadExcel.js
@@ -5,6 +5,6 @@ const excelFile = xlsx.readFile("bus.xlsx");
 const sheetName = excelFile.SheetNames[0];
 const firstSheet = excelFile.Sheets[sheetName];
 
-const jsonData = xlsx.utils.sheet_to_json( firstSheet, { defval : "" } );
+const jsonData = xlsx.utils.sheet_to_json(firstSheet, { defval: "" });
 
-console.log( jsonData );
+console.log(jsonData[0].도착);

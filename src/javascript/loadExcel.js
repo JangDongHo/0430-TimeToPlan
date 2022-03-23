e709802d03b2b89a1828707159d074f719398e12
Jang-DongHo <donghoo18@naver.com>
2022-03-23 21:45:06 +0900

    test

--- src/javascript/loadExcel.js
+++ src/javascript/loadExcel.js
@@ -1,18 +1,23 @@
-var xhr = new XMLHttpRequest();
-var url =
-  "https://api.odsay.com/v1/api/intercityServiceTime?startStationID=3601540&endStationID=4000255"; /*URL*/
-var queryParams =
-  "&" +
-  encodeURIComponent("apiKey") +
-  "=" +
-  "Jbsq50JZZESwIwDBrPu1sA"; /*Service Key*/
-xhr.open("GET", url + queryParams);
-console.log(url + queryParams);
-xhr.onreadystatechange = function () {
-  if (this.readyState == 4) {
-    busData = this.responseText;
-    console.log(busData.result);
-  }
-};
+const searchBarBtns = document.querySelector(".search-bar__btns");
 
-xhr.send("");
+function openSearchBar() {
+  var xhr = new XMLHttpRequest();
+  var url =
+    "https://api.odsay.com/v1/api/intercityServiceTime?startStationID=3601540&endStationID=4000255"; /*URL*/
+  var queryParams =
+    "&" +
+    encodeURIComponent("apiKey") +
+    "=" +
+    "Jbsq50JZZESwIwDBrPu1sA"; /*Service Key*/
+  xhr.open("GET", url + queryParams);
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      busData = this.responseText;
+      console.log(busData.result);
+    }
+  };
+
+  xhr.send("");
+}
+
+searchBtn.addEventListener("click", openSearchBar);

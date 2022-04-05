e2716bd4b5e1f4054f82289afe48857fbe7ebf30
Jang-DongHo <donghoo18@naver.com>
2022-04-05 21:18:34 +0900

    test

--- src/javascript/menuController.js
+++ src/javascript/menuController.js
@@ -1,10 +1,12 @@
-import getStationID from "./getBusInfo.js";
+//import getStationID from "./getBusInfo.js";
 
 const searchBtn = document.querySelector(".search-bar");
 const searchBarContainer = document.querySelector(".search-bar-container");
 const closeBtn = document.querySelector(".search-bar__close-btn");
 
 const busStopBtn = document.querySelector(".bus-stop__btn");
+
+let terminalName = "";
 
 /* SearchBar 컨트롤러 */
 function openSearchBar() {
@@ -18,10 +20,12 @@ function closeSearchBar() {
 /* busStationBtn 컨트롤러 */
 function renderBusInfo() {
   location.href = "https://homegnu.kro.kr/bus-info";
-  const terminalName = busStopBtn.firstChild.innerHTML;
+  terminalName = busStopBtn.firstChild.innerHTML;
 }
 
 searchBtn.addEventListener("click", openSearchBar);
 closeBtn.addEventListener("click", closeSearchBar);
 
 busStopBtn.addEventListener("click", renderBusInfo);
+
+export default terminalName;
